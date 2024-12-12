ade27cf0e31627dc84a9e47b613a86c12c8dc5ba
p_buian <p.buyan@gmail.com>
2024-12-12 01:57:36 -0500

    project upd

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,26 +1,6 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
-
-export const getURL = (input: string = "") => {
-    return process.env.NEXT_PUBLIC_VERCEL_ENV === "production"
-      ? `https://${process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL}${input}`
-      : process.env.NEXT_PUBLIC_VERCEL_ENV === "preview"
-        ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}${input}`
-        : `http://localhost:3000${input}`;
-  };
-
-  // run `pnpm tunnel` and set TUNNEL_URL
-  export const getDomain = (input: string = "") => {
-    const domain =
-      process.env.NEXT_PUBLIC_VERCEL_ENV === "production"
-        ? `https://${process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL}`
-        : process.env.NEXT_PUBLIC_VERCEL_ENV === "preview"
-          ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
-          : process.env.TUNNEL_URL!;
-  
-    return domain + input;
-  };
