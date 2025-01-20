1d7080f64a330033b9263769397f63e255d23dc3
Peter Bu <p.buyan@gmail.com>
2025-01-20 14:24:31 -0500

    callback upd

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -1,21 +1,40 @@
 import { NextResponse } from "next/server";
 // The client you created from the Server-Side Auth instructions
 import { createClient } from "@/utils/supabase/server";
+import { createStripeCustomer } from "@/utils/stripe/api";
+import { db } from "@/utils/db/db";
+import { usersTable } from "@/utils/db/schema";
+import { eq } from "drizzle-orm";
 
 export async function GET(request: Request) {
 	const { searchParams, origin } = new URL(request.url);
 	const code = searchParams.get("code");
-	// if "next" is in param, use it as the redirect URL
 	const next = searchParams.get("next") ?? "/";
 
 	if (code) {
-		const supabase = await createClient();
+		const supabase = createClient();
 		const { error } = await supabase.auth.exchangeCodeForSession(code);
 		if (!error) {
-			const forwardedHost = request.headers.get("x-forwarded-host"); // original origin before load balancer
+			const {
+				data: { user },
+			} = await supabase.auth.getUser();
+
+			const checkUserInDB = await db.select().from(usersTable).where(eq(usersTable.email, user!.email!));
+			const isUserInDB = checkUserInDB.length > 0 ? true : false;
+			if (!isUserInDB) {
+				const stripeID = await createStripeCustomer(user!.id, user!.email!, user!.user_metadata.full_name);
+				await db.insert(usersTable).values({
+					id: user!.id,
+					name: user!.user_metadata.full_name,
+					email: user!.email!,
+					stripe_id: stripeID,
+					plan: "none",
+				});
+			}
+
+			const forwardedHost = request.headers.get("x-forwarded-host");
 			const isLocalEnv = process.env.NODE_ENV === "development";
 			if (isLocalEnv) {
-				// we can be sure that there is no load balancer in between, so no need to watch for X-Forwarded-Host
 				return NextResponse.redirect(`${origin}${next}`);
 			} else if (forwardedHost) {
 				return NextResponse.redirect(`https://${forwardedHost}${next}`);
@@ -25,6 +44,5 @@ export async function GET(request: Request) {
 		}
 	}
 
-	// return the user to an error page with instructions
 	return NextResponse.redirect(`${origin}/auth/auth-code-error`);
 }
